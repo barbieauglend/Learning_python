88ffaa44936a4467f44def9b0bf24d4ff33f5f4f
barbieAuglend <barbieauglend@icloud.com>
2016-11-11 21:26:02 +0100

    Tweet!

--- RandomTweet/randomtweet.py
+++ RandomTweet/randomtweet.py
@@ -1,16 +1,10 @@
 from random import shuffle
 
-def tweeting(n):
-    tweetList = example[:n]
-    tweet =  ' '.join(tweetList)
-
 example = open("GPL.txt").read().split()
 shuffle(example)
 
 nwords = input("enter how many words you want: ")
-tweeting(nwords)
-
-while len(tweet) > 140:
-    tweeting(nwords)
+tweetList = example[:nwords]
+tweet = ' '.join(tweetList)
 
 print(tweet)
