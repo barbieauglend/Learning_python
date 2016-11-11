095fe8d75b8c03ce0220c3529ea7675a1c87200a
barbieauglend <barbieauglend@icloud.com>
2016-11-11 13:34:28 +0100

    Lvl 02 done =)

---
+++ Ladebalken/ladebalken_2.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+def progressbar(it, size):
+    count = len(it) + start
+
+    def _show(_i):
+        x = int(size * (_i + start) / count)
+        sys.stdout.write("[%s%s] %i %s\r" % ("#" * x, " " * (size - x), _i + start, '%'))
+        sys.stdout.flush()
+
+    _show(0)
+    for i, item in enumerate(it):
+        yield item
+        _show(i + 1)
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
+input_start = input("Enter State: ")
+start = int(input_start)
+
+for i in progressbar(range(100-start), 10):
+    time.sleep(0.1)
