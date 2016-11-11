b389448439879274fb13397d248024ab08ccbb40
barbieauglend <barbieauglend@icloud.com>
2016-11-11 14:29:00 +0100

    Calculator done =)

---
+++ Calculator/calc.py
@@ -0,0 +1,39 @@
+import sys
+
+def add(x, y):
+   return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+   return x / y
+
+input_first  = input("First number: ")
+x = int(input_first)
+
+input_op  = raw_input("Op: ")
+op = str(input_op)
+
+input_second = input("Second number: ")
+y = int(input_second)
+
+if op == "+":
+    res = add(x,y)
+
+elif op == "-":
+    res = subtract(x,y)
+
+elif op == "*":
+    res = multiply(x,y)
+
+elif op == "/":
+    res = divide(x,y)
+
+else: print("Not a valid input")
+
+sys.stdout.write("%i %s %i %s %i" % (x, op, y, '=', res))
+sys.stdout.write("\n")
