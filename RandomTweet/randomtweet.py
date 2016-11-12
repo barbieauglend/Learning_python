90fd0a03e4a79e8a229be78bdc37056b60e1cc13
barbieAuglend <barbieauglend@icloud.com>
2016-11-12 18:09:04 +0100

    Python 3 Version geadded

--- RandomTweet/randomtweet.py
+++ RandomTweet/randomtweet.py
@@ -1,10 +1,26 @@
+#python 2.7
+
+# from random import shuffle
+#
+# example = open("GPL.txt").read().split()
+# shuffle(example)
+#
+# nwords = input("enter how many words you want: ")
+# tweetList = example[:nwords]
+# tweet = ' '.join(tweetList)
+#
+# print(tweet)
+
+#python 3.5
+
 from random import shuffle
 
 example = open("GPL.txt").read().split()
 shuffle(example)
 
 nwords = input("enter how many words you want: ")
-tweetList = example[:nwords]
+n = int(nwords)
+tweetList = example[:n]
 tweet = ' '.join(tweetList)
 
 print(tweet)
