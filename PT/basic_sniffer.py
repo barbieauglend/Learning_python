3399d636574359a3597f3c3fe38f8d4a1d84c055
barbieAuglend <barbieauglend@icloud.com>
2017-04-13 08:46:27 +0200

    Sniffing

---
+++ PT/basic_sniffer.py
@@ -0,0 +1,15 @@
+import socket
+
+# Create he sniffer socket
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+
+# Bind to localhost
+sniffer.bind(('0.0.0.0', 0))
+
+# IP header Check
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+print('sniffer is listening for incoming connections')
+
+# Get a single packet
+print(sniffer.recvfrom(65535))
